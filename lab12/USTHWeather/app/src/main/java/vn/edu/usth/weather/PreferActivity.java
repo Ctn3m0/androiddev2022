8fc610053f197342c50f677ee3272d22efcf8d9d
ctn3m0 <hieuquang15801@gmail.com>
2021-10-08 09:44:34 +0700

    Labwork 12

---
+++ lab12/USTHWeather/app/src/main/java/vn/edu/usth/weather/PreferActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prefer);
+    }
+}
